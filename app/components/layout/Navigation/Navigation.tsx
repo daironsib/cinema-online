72351c8e8630de4901dd6de899d6ff5a37b9597d
Artem Sokolov <daironsib@gmail.com>
2023-10-27 11:05:58 +0200

    Add main layout

---
+++ app/components/layout/Navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import styles from './Navigation.module.scss'
+
+const Navigation: FC = () => {
+  return (
+    <div>
+      Sidebar
+    </div>
+  )
+}
+
+export default Navigation
