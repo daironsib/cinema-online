14d5def676618f3808d9d358a14fadea11f29196
Artem Sokolov <daironsib@gmail.com>
2023-10-28 17:56:57 +0200

    Add genres to menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import { NextPage } from 'next'
 
 import Home from '@/components/screens/home/Home'
+import MainProvider from 'providers/MainProvider'
 
 const HomePage: NextPage = () => {
-  return <Home />
+	return (
+		<MainProvider>
+			<Home />
+		</MainProvider>
+	)
 }
 
 export default HomePage
